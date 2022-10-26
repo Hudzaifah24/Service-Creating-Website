efdb1fc12dd4cf5ab23f0dbf5ec29e81dc41329b
Hudzaifah <hudz1357@gmail.com>
2022-10-26 14:24:24 +0700

    second commit

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: 'six-red-stars-technology',
+  // base: 'six-red-stars-technology',
 });
